3269a3e105cd10dac47028457db1891909f35647
Kyant0 <kyant2021@outlook.com>
2025-10-15 15:55:45 +0800

    Use new method to set jvmToolchain

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -9,7 +7,7 @@ plugins {
 android {
     namespace = "com.kyant.capsule.demo"
     compileSdk = 36
-    buildToolsVersion = "36.0.0"
+    buildToolsVersion = "36.1.0"
 
     defaultConfig {
         applicationId = "com.kyant.capsule.demo"
@@ -27,17 +25,8 @@ android {
             vcsInfo.include = false
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
-    }
     kotlin {
-        compilerOptions {
-            jvmTarget = JvmTarget.JVM_21
-            freeCompilerArgs.addAll(
-                "-jvm-default=no-compatibility",
-            )
-        }
+        jvmToolchain(21)
     }
     buildFeatures {
         compose = true
