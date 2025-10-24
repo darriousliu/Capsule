75b415f281a27ab245eb39aafe18276a4d4a2c52
Kyant0 <kyant2021@outlook.com>
2025-10-24 11:49:07 +0800

    Configure maven central publish

--- capsule/build.gradle.kts
+++ capsule/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.vanniktech.maven.publish")
 }
 
 android {
@@ -33,4 +34,37 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.foundation)
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    coordinates("io.github.kyant0", "capsule", "2.1.1")
+
+    pom {
+        name.set("Capsule")
+        description.set("Jetpack Compose smooth corners")
+        inceptionYear.set("2025")
+        url.set("https://github.com/Kyant0/Capsule")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("repo")
+            }
+        }
+        developers {
+            developer {
+                id.set("Kyant0")
+                name.set("Kyant")
+                url.set("https://github.com/Kyant0")
+            }
+        }
+        scm {
+            url.set("https://github.com/Kyant0/Capsule")
+            connection.set("scm:git:git://github.com/Kyant0/Capsule.git")
+            developerConnection.set("scm:git:ssh://git@github.com/Kyant0/Capsule.git")
+        }
+    }
 }
